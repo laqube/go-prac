63a9467459cd23357afbf8a71de524a389ab2baa
Miras <91594617+laqube@users.noreply.github.com>
2023-03-14 11:37:46 +0600

    feat | ws endpoint added
    server-side changes featuring addition of websocket endpoint

--- backend/main.go
+++ backend/main.go
@@ -11,22 +11,22 @@ import (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
-	CheckOrigin:     func(r *http.Request) bool { return true },
+
+	CheckOrigin: func(r *http.Request) bool { return true },
 }
 
 func reader(conn *websocket.Conn) {
 	for {
 		messageType, p, err := conn.ReadMessage()
 		if err != nil {
-			log.Println(err)
+			log.Printl(err)
 			return
 		}
-		fmt.Println(string(p))
-
-		if err := conn.WriteMessage(messageType, p); err != nil {
-			log.Println(err)
-			return
-		}
+	}
+	fmt.Println(string(p))
+	if err := conn.WriteMessage(messageType, p); err != nil {
+		log.Println(err)
+		return
 	}
 }
 
@@ -41,11 +41,15 @@ func serveWs(w http.ResponseWriter, r *http.Request) {
 
 func setupRoutes() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Simple Server")
+		fmt.Fprint(w, "Qarapaiym server")
 	})
+
+	http.HandleFunc("/ws", serveWs)
 }
 
 func main() {
+	fmt.Println("Chat App v0.01")
 	setupRoutes()
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":5050", nil)
+
 }
